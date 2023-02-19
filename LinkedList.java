8ab62ba60e625f4fc3e13ed6bc762ea01d55ec5d
Parvsub <Praveenboda421@gmail.com>
2023-02-19 21:38:13 +0530

    Added: Delete the last element

--- LinkedList.java
+++ LinkedList.java
@@ -1,81 +1,50 @@
 package com.Linkedlist;
 
 
-public class LinkedList<T> {
-    public Node<T> head;
-    public Node<T> tail;
 
-    public void push(T data) {
-        Node<T> node = new Node<>(data);
-        if (head == null) {
-            head = node;
-            tail = node;
-        } else {
-            node.setNext(head);
-            head = node;
-        }
-    }
-    public void add(T data){
-        Node<T> newnode=new Node<>(data);
-        if (head==null) {
-            head = newnode;
-            tail = newnode;
-        }else {
-            tail.setNext(newnode);
-            tail=newnode;
-        }
-    }
-    public Node<T> search(T searchData) {
-        Node<T> temp = head;
-        while (temp != null) {
-            if (temp.getData().equals(searchData))
-                return temp;
-            temp = (Node<T>) temp.getNext();
-        }
-        return null;
-    }
+class  DeleteThelastelement{
+    static class Node {
+        int data;
+        Node next;
+    };
 
-    public boolean searchAndInsert(T searchData, T insertData) {
-        Node<T> newNode = new Node(insertData);
-        Node<T> searchedNode = search(searchData);
-        if (searchedNode == null)
-            return false;
-        else {
-            newNode.setNext(searchedNode.getNext());
-            searchedNode.setNext(newNode);
-            return true;
-        }
-
-    }
-    public T pop(){
-        if (head==null) {
+    static Node removeLastNode(Node head)
+    {
+        if (head == null)
             return null;
-        }else {
-            T data=head.getData();
-            head=(Node<T>) head.getNext();
-            return data;
+
+        if (head.next == null) {
+            return null;
         }
+
+        Node second_last = head;
+        while (second_last.next.next != null)
+            second_last = second_last.next;
+
+        second_last.next = null;
+
+        return head;
     }
-    public void show(){
-        if (head==null){
-            System.out.println("List is Empty");
-        }else {
-            Node<T> temp = head;
-            while (temp != null) {
-                System.out.print(temp.getData()+" ");
-                temp = (Node<T>) temp.getNext();
-            }
-            System.out.println();
-        }
-    }
-    public boolean isEmpty(){
-        return head==null ? true:false;
+    static Node push(Node head_ref, int new_data)
+    {
+        Node new_node = new Node();
+        new_node.data = new_data;
+        new_node.next = (head_ref);
+        (head_ref) = new_node;
+        return head_ref;
     }
 
-    @Override
-    public String toString() {
-        return "LinkedList{" +
-                "head=" + head +
-                '}';
+    // Driver code
+    public static void main(String args[])
+    {
+        Node head = null;
+        head = push(head, 70);
+        head = push(head, 30);
+        head = push(head, 56);
+
+
+        head = removeLastNode(head);
+        for (Node temp = head; temp != null; temp = temp.next)
+            System.out.print(temp.data + " ");
     }
 }
